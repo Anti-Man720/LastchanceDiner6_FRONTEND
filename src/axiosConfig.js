e1156c3d9c824e8a6c799266b08015344abb9288
M~dot~Carter <120771453+Anti-Man720@users.noreply.github.com>
2023-10-16 22:59:20 -0500

    new .env url

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
 axios.defaults.baseURL =
-  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : '/';
+  process.env.NODE_ENV !== 'production' ? 'https://capstone-backend2-70ebab3d848f.herokuapp.com/' : '/';
