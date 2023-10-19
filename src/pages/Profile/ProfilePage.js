73c91237c017ba1e374ffff6573706034e04b1e9
M~dot~Carter <120771453+Anti-Man720@users.noreply.github.com>
2023-10-19 07:44:58 -0500

    cp file location

--- src/pages/Profile/ProfilePage.js
+++ src/pages/Profile/ProfilePage.js
@@ -5,7 +5,7 @@ import classes from './profilePage.module.css';
 import Title from '../../components/Title/Title';
 import Input from '../../components/Input/Input';
 import Button from '../../components/Button/Button';
-import ChangePassword from '../components/ChangePassword/ChangePassword';
+import ChangePassword from '../../components/ChangePassword/ChangePassword';
 
 export default function ProfilePage() {
   const {
